0cf14164aa77485bc71769f0c12bc78cd21ce4d3
sevenfate <7fate@web.de>
2016-09-18 13:52:14 +0200

    First Draft

---
+++ src/main/java/com/e2open/datahub/E2openE2DhApplication.java
@@ -0,0 +1,12 @@
+package com.e2open.datahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2openE2DhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(E2openE2DhApplication.class, args);
+    }
+}
