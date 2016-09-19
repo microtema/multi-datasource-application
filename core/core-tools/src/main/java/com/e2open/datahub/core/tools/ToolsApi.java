564979ecc28e840cacd7328a3305c73bb4cd4b16
sevenfate <7fate@web.de>
2016-09-19 17:06:24 +0200

    Add Multiple Datasource

--- core/core-tools/src/main/java/com/e2open/datahub/core/tools/ToolsApi.java
+++ core/core-tools/src/main/java/com/e2open/datahub/core/tools/ToolsApi.java
@@ -1,4 +1,4 @@
-package com.e2open.datahub.core;
+package com.e2open.datahub.core.tools;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 
 @Component
-public class CoreApi {
+public class ToolsApi {
 
-    private static final Logger LOGGER = LogManager.getLogger(CoreApi.class);
+    private static final Logger LOGGER = LogManager.getLogger(ToolsApi.class);
 
     @Value("${com.e2open.datahub.core.api}")
     private String property;
--- core/webui/src/main/java/com/e2open/datahub/DXApplication.java
+++ core/webui/src/main/java/com/e2open/datahub/DXApplication.java
@@ -2,9 +2,15 @@ package com.e2open.datahub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -17,6 +23,21 @@ public class DXApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(DXApplication.class);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "e2datahub.metadata.datasource")
+    public DataSource metadataDataSource() {
+
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "e2datahub.staging.datasource")
+    public DataSource stagingDataSource() {
+
+        return DataSourceBuilder.create().build();
     }
 
 }
